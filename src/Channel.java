749c1c0bd27520d17bd99b1356d2bfdaa0d97dc5
Mohamed Ahmed Mokhtar <mohamedAhmedMokhtarElkomy>
2022-12-28 04:18:41 +0200

    add 2 missing classes(Channel, PacketType)

---
+++ src/Channel.java
@@ -0,0 +1,11 @@
+public class Channel {
+    private boolean isBusy = false;
+
+    public boolean isBusy() {
+        return isBusy;
+    }
+
+    public void setBusy(boolean busy) {
+        isBusy = busy;
+    }
+}
---
+++ src/enums/PacketType.java
@@ -0,0 +1,9 @@
+package enums;
+
+public enum PacketType {
+    RTS,
+    PKT,
+    CTS,
+
+    ACK,
+}
